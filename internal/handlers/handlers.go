70735d1a96fe27b5a295b6102141273e1de6e637
Dylan Parker <dylan.parker@codecave.app>
2019-06-07 12:32:09 -0500

    new change

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -17,6 +17,6 @@ func New() (*Server, error) {
 // Ping ...
 func (s *Server) Ping(context.Context, *protobuf.PingRequest) (*protobuf.PingResponse, error) {
 	return &protobuf.PingResponse{
-		Message: "jakob broke this",
+		Message: "test update",
 	}, nil
 }
